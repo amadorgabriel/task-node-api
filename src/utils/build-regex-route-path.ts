440db6fbff91c38515cf725b750b9aef58b0316e
amadorgabriel <gabrielramador2014@gmail.com>
2023-06-05 15:48:02 -0300

    feat: add utils

---
+++ src/utils/build-regex-route-path.ts
@@ -0,0 +1,8 @@
+export function buildRegexRoutePath(path: string) {
+  const routeParamsRegexPattern = /:([a-zA-Z])+/g
+  const paramsWithRegex = path.replace(routeParamsRegexPattern, '(?<$1>[a-z0-9\-_]+)')
+
+  const regexWithQueryParameters = new RegExp(`^${paramsWithRegex}(?<query>\\?(.*))?$`) 
+
+  return regexWithQueryParameters
+}
